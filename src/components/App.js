4ddf1e982ab3cee9540e57314bc1139853dadca0
Cheri Portman <cheriportman@hotmail.com>
2019-05-20 11:42:09 -0700

    App.js written, snapshot taken

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Red from './Red';
+import Blue from './Blue';
+import Yellow from './Yellow';
+import Header from './Header';
+
+export default function App() {
+  return (
+    <Router>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/red" component={Red} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/yellow" componenet={Yellow} />
+        </Switch>
+      </>
+    </Router>
+  );
+}
